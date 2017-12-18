861c0c8c998fe6dbddb960fa598342e0ca8495b4
rajasany <34409632+rajasany@users.noreply.github.com>
2017-12-18 23:03:27 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,98 @@
+﻿require('rootpath')();
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var expressJwt = require('express-jwt');
+var config = require('config.json');
+const http = require('http');
+const path = require('path');
+var mongoose = require('mongoose'),// MongoDB connection library
+	passport = require('passport'),// Authentication framework
+    LocalStrategy = require('passport-local').Strategy,
+    expressValidator = require('express-validator'), // validation tool for processing user input
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    MongoStore = require('connect-mongo/es5')(session), // store sessions in MongoDB for persistence
+    bcrypt = require('bcrypt'), // middleware to encrypt/decrypt passwords
+    sessionDB,
+    cfenv = require('cfenv'),// Cloud Foundry Environment Variables
+       appEnv = cfenv.getAppEnv();// Grab environment variables
+
+/********************************
+Local Environment Variables
+ ********************************/
+if(appEnv.isLocal){
+    require('dotenv').load();// Loads .env file into environment
+}
+
+/******************************** 
+ MongoDB Connection
+ ********************************/
+
+//Detects environment and connects to appropriate DB
+if(appEnv.isLocal){
+    mongoose.connect(process.env.LOCAL_MONGODB_URL);
+    sessionDB = process.env.LOCAL_MONGODB_URL;
+    console.log('Your MongoDB is running at ' + process.env.LOCAL_MONGODB_URL);
+}
+// Connect to MongoDB Service on Bluemix
+else if(!appEnv.isLocal) {
+    var mongoDbUrl, mongoDbOptions = {};
+    var mongoDbCredentials = appEnv.services["compose-for-mongodb"][0].credentials;
+    var ca = [new Buffer(mongoDbCredentials.ca_certificate_base64, 'base64')];
+    mongoDbUrl = mongoDbCredentials.uri;
+    mongoDbOptions = {
+      mongos: {
+        ssl: true,
+        sslValidate: true,
+        sslCA: ca,
+        poolSize: 1,
+        reconnectTries: 1
+      }
+    };
+
+    console.log("Your MongoDB is running at ", mongoDbUrl);
+    mongoose.connect(mongoDbUrl, mongoDbOptions); // connect to our database
+    sessionDB = mongoDbUrl;
+}
+else{
+    console.log('Unable to connect to MongoDB.');
+}
+	   
+	   
+// Angular DIST output folder
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// use JWT auth to secure the api, the token can be passed in the authorization header or querystring
+app.use(expressJwt({
+    secret: config.secret,
+    getToken: function (req) {
+        if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+            return req.headers.authorization.split(' ')[1];
+        } else if (req.query && req.query.token) {
+            return req.query.token;
+        }
+        return null;
+    }
+}).unless({ path: ['/users/authenticate', '/users/register', '/users/check'] }));
+
+// routes
+app.use('/users', require('./controllers/users.controller'));
+
+// Send all other requests to the Angular app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+//Set Port
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Running on localhost:${port}`));
